55ad95f2c86b8f7a66c9ca2194f9867581118589
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-06-13 10:43:35 +0500

    boshladik

---
+++ scripts.jsx
@@ -0,0 +1,48 @@
+//  _______________ >< Redux >< _______________ //
+
+const { compose } = require("async");
+
+//  _______________ >< (1) Redux and Compose method >< _______________ //
+import { compose } from "redux";
+
+// const WorldCup = (props) => {
+//   const qush = (val)=>{
+//     return val+10
+//   }
+
+//   const kupay = (val)=>{
+//     return val+20
+//   }
+
+//   const ayir = (val)=>{
+//     return val+30
+//   }
+
+//   const natija = compose(qush,kupay,ayir)
+//   const natija2 = natija(10)
+// }
+
+// Compose methodi bir nechta funcsiyalarni bitta funcsiyasiga yig'ib beradi.
+
+//  _______________ >< (2) Create store methodi va reduserlar >< _______________ //
+
+const WorldCup = (props) => {
+
+
+  const natija = compose(qush, kupay, ayir);
+  const natija2 = natija(10);
+};
+
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
+//  _______________ >< Redux >< _______________ //
---
+++ src/App.js
@@ -0,0 +1,55 @@
+//  _______________ >< Redux >< _______________ //
+import "./App.css";
+const { compose } = require("async");
+import { compose } from "redux";
+
+function App() {
+  return <div className="App"></div>;
+
+  //  _______________ >< (1) Redux and Compose method >< _______________ //
+
+  // const WorldCup = (props) => {
+  //   const qush = (val)=>{
+  //     return val+10
+  //   }
+
+  //   const kupay = (val)=>{
+  //     return val+20
+  //   }
+
+  //   const ayir = (val)=>{
+  //     return val+30
+  //   }
+
+  //   const natija = compose(qush,kupay,ayir)
+  //   const natija2 = natija(10)
+  // }
+
+  // Compose methodi bir nechta funcsiyalarni bitta funcsiyasiga yig'ib beradi.
+
+  //  _______________ >< (2) Create store methodi va reduserlar >< _______________ //
+
+  // Reduser bu markazlashtirilgan flechkada turgan ma'lumotlarni oladi va qo'shimcha yangi ma'lumotlarni qayta fleshkaga saqlaydi
+
+  // const WorldCup = (props) => {
+
+  //   const natija = compose(qush, kupay, ayir);
+  //   const natija2 = natija(10);
+  // };
+
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+  //  _______________ >< Redux >< _______________ //
+}
+
+export default App;
