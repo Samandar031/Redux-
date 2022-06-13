4b556068417f439dfa465a9443b73a755f43fd5b
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-06-13 10:53:42 +0500

    Action creator and Dispatch

--- src/App.js
+++ src/App.js
@@ -30,14 +30,47 @@ function App() {
   //  _______________ >< (2) Create store methodi va reduserlar >< _______________ //
 
   // Reduser bu markazlashtirilgan flechkada turgan ma'lumotlarni oladi va qo'shimcha yangi ma'lumotlarni qayta fleshkaga saqlaydi
+  // Reduserga repleace methodi o'xshab ketadi. Reduser funcksiya ko'rinishida yaratiladi (errov funcksiya) va bu funcsiyaga 2 ta argument kiradi
+  // birinchi argument fleshkada turgan ma'lumotlar, ikkinchi argumenti qo'shimcha biz berayotgan ma'lumotlar.
 
-  // const WorldCup = (props) => {
+  // Create store methodi ma'lumotlarni saqlab turish uchun markazlashtirilgan fleshka yaratib beradi. Bu methodga 3 ta argument kiradi:
+  // 1- Reduceri yaratish uchun funcksiya, 2- Initial state, 3- Middleware (enhanser).
+  // 2) Initial state - ma'lumotlarni yaratish uchun qo'shimcha ma'lumotlar.
+  // 3) enhanser - redux da aylanayotgan malumotlarni manitoring qilib boradi
+  // reduser har doim nimadir return qilish kk
 
-  //   const natija = compose(qush, kupay, ayir);
-  //   const natija2 = natija(10);
+  // console.clear();
+
+  // const initialState = {
+  //   name: "Umid",
   // };
 
-  //  _______________ >< Redux >< _______________ //
+  // const changeNameReducer = (state, action) => {
+  //   if (1 > 6) {
+  //     return {
+  //       name: "salom",
+  //       age: 20,
+  //       eskisi: {
+  //         state,
+  //       },
+  //     };
+  //   }
+  //   return state;
+  // };
+
+  // const ali = {
+  //   name: "Samandar",
+  // };
+  // const vali = {
+  //   age: 25,
+  // };
+
+  // const store = Redux.createStore(changeNameReducer, initialState);
+  // const stor = Redux.createStore(changeNameReducer, vali);
+  // console.log(stor.getState());
+
+  //  _______________ >< Action Creater and Dispatch >< _______________ //
+
   //  _______________ >< Redux >< _______________ //
   //  _______________ >< Redux >< _______________ //
   //  _______________ >< Redux >< _______________ //
